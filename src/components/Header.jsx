eb08ef0984f42735a4fc96d570e125d1245d6f35
leksander3 <43728108+leksander3@users.noreply.github.com>
2022-04-30 10:00:52 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png" />
+    </header>
+  );
+};
+
+export default Header;
