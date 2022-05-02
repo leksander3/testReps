5816b8e9a677d36003479a4d623bd8aeacadcf8e
leksander3 <43728108+leksander3@users.noreply.github.com>
2022-05-02 16:14:44 +0300

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Header.modul.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png" />
     </header>
   );
