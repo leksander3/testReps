5148662b0012a9885f546d714a253a8345067710
leksander3 <43728108+leksander3@users.noreply.github.com>
2022-05-30 21:48:20 +0300

    like

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import s from "./MyPosts.module.css";
+import Post from "./Posts/Post";
 
 const MyPosts = () => {
   return (
     <div>
       <div>
         my posts
-        <div>new posts</div>
+        <div>
+          <textarea></textarea>
+          <button>Add posts</button>
+          <button>Remove</button>
+        </div>
       </div>
       <div className={s.posts}>
-        <div className={s.item}>post 1</div>
-        <div className={s.item}>post 2</div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
